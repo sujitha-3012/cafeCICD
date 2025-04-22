852433ff8d4c7eb2db3d28fd4fbe123c54c9e964
sujitha-3012 <sujithasvpssirigiri@gmail.com>
2025-04-22 11:10:44 +0530

    Update getAppParameters.php

--- cafe/getAppParameters.php
+++ cafe/getAppParameters.php
@@ -1,42 +1,13 @@
 <?php
 
-require 'aws.phar'; #AWSSDK
-#require 'aws-autoloader.php';
-$ch = curl_init();
+require 'aws.phar'; // AWSSDK
 
-// get a valid TOKEN
-$headers = array (
-        'X-aws-ec2-metadata-token-ttl-seconds: 21600' );
-$url = "http://169.254.169.254/latest/api/token";
-#echo "URL ==> " .  $url;
-curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
-curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, "PUT" );
-curl_setopt( $ch, CURLOPT_URL, $url );
-$token = curl_exec( $ch );
-
-#echo "<p> TOKEN :" . $token;
-// then get metadata of the current instance 
-$headers = array (
-        'X-aws-ec2-metadata-token: '.$token );
-
-$url = "http://169.254.169.254/latest/meta-data/placement/availability-zone";
-
-curl_setopt( $ch, CURLOPT_URL, $url );
-curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
-curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, "GET" );
-$result = curl_exec( $ch );
-$az = curl_exec( $ch );
-
-#echo "<p> RESULT :" . $result;
-
-$region = substr($az, 0, -1);
+// Get region from environment variable or default to 'us-east-2'
+$region = getenv('AWS_REGION') ?: 'us-east-2';
 
 $secrets_client = new Aws\SecretsManager\SecretsManagerClient([
   'version' => 'latest',
   'region'  => $region,
-  'version' => '2017-10-17'
 ]);
 
 $showServerInfo = "";
@@ -49,31 +20,37 @@ $db_password = "";
 
 try {
   $db_url = $secrets_client->getSecretValue([
-    'SecretId' => '/cafe/dbUrl'
+    'SecretId' => '/cafe/dbUrl1'
   ]);
   $db_url = $db_url["SecretString"];
+
   $db_user = $secrets_client->getSecretValue([
-    'SecretId' => '/cafe/dbUser'
+    'SecretId' => '/cafe/dbUser1'
   ]);
   $db_user = $db_user["SecretString"];
+
   $db_password = $secrets_client->getSecretValue([
-    'SecretId' => '/cafe/dbPassword'
+    'SecretId' => '/cafe/dbPassword1'
   ]);
   $db_password = $db_password["SecretString"];
+
   $db_name = $secrets_client->getSecretValue([
-    'SecretId' => '/cafe/dbName'
+    'SecretId' => '/cafe/dbName1'
   ]);
   $db_name = $db_name["SecretString"];
+
   $currency = $secrets_client->getSecretValue([
-    'SecretId' => '/cafe/currency'
+    'SecretId' => '/cafe/currency1'
   ]);
   $currency = $currency["SecretString"];  
+
   $timezone = $secrets_client->getSecretValue([
-    'SecretId' => '/cafe/timeZone'
+    'SecretId' => '/cafe/timeZone1'
   ]);
   $timezone = $timezone["SecretString"];  
+
   $showServerInfo = $secrets_client->getSecretValue([
-    'SecretId' => '/cafe/showServerInfo'
+    'SecretId' => '/cafe/showServerInfo1'
   ]);
   $showServerInfo = $showServerInfo["SecretString"];  
 
@@ -87,5 +64,4 @@ catch (Exception $e) {
   $timeZone = '';
   $currency = '';
 }
-#error_log('Settings are: ' . $ep. " / " . $db_name . " / " . $db_user . " / " . $db_password);
 ?>
